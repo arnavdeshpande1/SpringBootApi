e74d7c88b11da00692c60db788d1b7fc31a445cf
Kedlaya Avinash Yajnanarayana <kayajnan@in.ibm.com>
2022-12-04 14:43:08 +0530

    testConfigMap

--- src/main/java/com/demo/api/endpoint/EndpointApplication.java
+++ src/main/java/com/demo/api/endpoint/EndpointApplication.java
@@ -3,14 +3,11 @@ package com.demo.api.endpoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import java.util.UUID;
 
 
 @RestController
@@ -18,15 +15,18 @@ public class EndpointApplication {
 	private static final Logger LOG = LoggerFactory.getLogger(EndpointApplication.class);
 
 	private final Environment environment;
+	private static String instanceId;
 
 	@Autowired
 	public EndpointApplication(Environment environment) {
 		this.environment = environment;
+		this.instanceId = UUID.randomUUID().toString();
 	}
 
 	@RequestMapping("/getMessage")
 	public String getMessage() {
 		LOG.info("App environment message: {}", environment.getProperty("instance.message"));
-		return environment.getProperty("instance.message");
+		LOG.info("App environment instanceId: {}", instanceId);
+		return environment.getProperty("instance.message") + ":" + instanceId;
 	}
 }
